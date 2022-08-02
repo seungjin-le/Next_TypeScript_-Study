8a6a4d2a9db06a2ef0a594674187ebd4d33ba477
Seungjin Lee <dltmdwls154@gmail.com>
2022-08-03 00:22:37 +0900

    loginModal UI 퍼블리싱

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,14 @@
-import { CButton, CCloseButton, CCol, CRow } from '@coreui/react';
+import {
+  CButton,
+  CCloseButton,
+  CCol,
+  CFormSwitch,
+  CModal,
+  CModalBody, CModalFooter,
+  CModalHeader,
+  CModalTitle,
+  CRow
+} from '@coreui/react';
 import { CCollapse } from '@coreui/react';
 import { CDropdown } from '@coreui/react';
 import { CDropdownItem } from '@coreui/react';
@@ -10,19 +20,24 @@ import { CHeader } from '@coreui/react';
 import { CHeaderNav } from '@coreui/react';
 import { CNavItem } from '@coreui/react';
 import { CNavLink } from '@coreui/react';
-import React, {useState} from 'react';
+import React, {ReactNode, useState} from 'react';
 import {CDropdownDivider, CDropdownToggle, CHeaderBrand} from '@coreui/react'
+import Modal from '../modal/Modal'
 
 const Header = () => {
   const [visible, setVisible] = useState(false)
-
+  const [darkMode, setDarkMode] = useState(false);
+  const [loginModal, setLoginModal] = useState(false);
+  const onChange = () => {
+    setDarkMode(!darkMode)
+  }
   return (
     <CHeader style={{minHeight:'auto'}} className='header'>
       <CContainer fluid>
         <CHeaderBrand>Bibibig</CHeaderBrand>
-        <CButton type="button" color={ visible ? 'danger' :'success'} onClick={() => setVisible(!visible)}>
-          {visible ? 'Close Menu' : 'Open Menu'}
-        </CButton>
+          <CButton type="button" color={ visible ? 'danger' :'success'} onClick={() => setVisible(!visible)}>
+            {visible ? 'Close Menu' : 'Open Menu'}
+          </CButton>
       </CContainer>
       <div className='menuBox'>
         <div className='menu'>
@@ -36,10 +51,22 @@ const Header = () => {
             <CNavItem>
               <CNavLink href="/">Link</CNavLink>
             </CNavItem>
-            <CDropdown variant="nav-item">
+            <CDropdown variant="nav-item" autoClose='outside'>
               <CDropdownToggle color="secondary">Menu</CDropdownToggle>
               <CDropdownMenu>
-                <CDropdownItem href="#">Action</CDropdownItem>
+                <CDropdownItem
+                  component='button'
+                  onClick={() => setLoginModal(!loginModal)}
+                >Login
+                </CDropdownItem>
+                <CDropdownItem component='button'>
+                  <CFormSwitch
+                    onChange={onChange}
+                    type='checkbox'
+                    checked={darkMode}
+                    label="Dark Mode"
+                    id="formSwitchCheckChecked" />
+                </CDropdownItem>
                 <CDropdownItem href="#">Another action</CDropdownItem>
                 <CDropdownDivider />
                 <CDropdownItem href="#">Something else here</CDropdownItem>
@@ -55,6 +82,18 @@ const Header = () => {
         </CCollapse>
         </div>
       </div>
+      <CModal visible={loginModal} onClose={() => setLoginModal(false)}>
+        <CModalHeader onClose={() => setLoginModal(false)}>
+          <CModalTitle>Login</CModalTitle>
+        </CModalHeader>
+        <CModalBody>Woohoo, youre reading this text in a modal!</CModalBody>
+        <CModalFooter>
+          <CButton color="primary">Login</CButton>
+          <CButton color="secondary" onClick={() => setLoginModal(false)}>
+            Close
+          </CButton>
+        </CModalFooter>
+      </CModal>
     </CHeader>
   );
 };
---
+++ components/modal/Modal.tsx
@@ -0,0 +1,16 @@
+import React, {ReactNode} from 'react';
+import {CButton, CModal, CModalBody, CModalFooter, CModalHeader, CModalTitle} from '@coreui/react'
+
+type Children = {
+  children?: ReactNode | ReactNode[],
+  props?: any
+}
+
+const Modal = ({children,props} : Children) => {
+
+  return (
+
+  );
+};
+
+export default Modal;
