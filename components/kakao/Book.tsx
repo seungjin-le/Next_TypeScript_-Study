c2227cf542c2279485f9d86e8ed022c0ebd333a3
Seungjin Lee <dltmdwls154@gmail.com>
2022-08-09 00:17:25 +0900

    Kakao Book Api coreUI 적용중

--- components/kakao/Book.tsx
+++ components/kakao/Book.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import {CButton, CCard, CCardBody, CCardImage, CCardText, CCardTitle} from '@coreui/react'
+
+type Props = {
+  props?:object
+  index:number
+  value:object
+}
+type valueObj = {
+  authors: Array<string>,
+  contents: string,
+  datetime: string,
+  isbn: string,
+  price: number,
+  publisher: string,
+  sale_price: number,
+  status: string,
+  thumbnail: string
+  title: string,
+  translators: Array<string>
+  url: string
+}
 
 
-
-const Books = () => {
+const Books = (value : valueObj, index : Props) => {
+  console.log(value, index)
+  //const {price} = value
+  //{
 
   return (
-    <div>
-
-    </div>
+    <CCard style={{ width: '18rem' }}>
+      <CCardImage orientation="top" src={value?.thumbnail} />
+      <CCardBody>
+        <CCardTitle>{value.title}</CCardTitle>
+        <CCardText>
+          {value.contents}
+        </CCardText>
+        <CButton href="#">Go somewhere</CButton>
+      </CCardBody>
+    </CCard>
   );
 };
 
--- pages/kakao/KakaoBook.tsx
+++ pages/kakao/KakaoBook.tsx
@@ -1,50 +1,56 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Book from '../../components/kakao/Book'
 import axios from 'axios'
+
 
 type Props = {
   props : any
 }
-//6ae0ee0f0fbe16305169e75c70ef2eaa
+
 const KakaoBook = ({props} : Props) => {
-  console.log(process.env['KAKAO_BOOK_API'])
-  console.log(props)
+  const [data, setData] : any[]= useState();
+
+  useEffect(() => {
+    if(props){
+      setData(Object.keys(props).map(item => props[item]))
+    }
+  }, [props]);
 
   return (
     <div>
-      <Book />
+      {data &&
+        data.map((value : object, index : number) => (
+          <Book value={value} index={index} key={index}/>
+        ))
+      }
     </div>
   );
 };
 export async function getServerSideProps() {
 
-  const data = axios.get("https://dapi.kakao.com/v3/search/book?target=title",{
+  const data = await axios.get("https://dapi.kakao.com/v3/search/book?target=title", {
     params: {
       // 검색어
       query: '미움받을 용기'
-
       // 필수아닌 검색 조건들
-
       //결과 문서 정렬 방식
       //sort	String	, accuracy(정확도순) 또는 latest(발간일순), 기본값 accuracy
-
       //결과 페이지 번호
       //page	Integer	, 1~50 사이의 값, 기본 값 1
-
       //한 페이지에 보여질 문서 수
       //size	Integer	, 1~50 사이의 값, 기본 값 10
-
       //target	String	검색 필드 제한
       //사용 가능한 값: title(제목), isbn (ISBN), publisher(출판사), person(인명)
     },
     headers: {
-      "Authorization": `KakaoAK c632ee2baf4d5a2bbfa3277ea498e550`
+      "Authorization": `KakaoAK ${process.env['KAKAO_BOOK_API']}`
     },
   })
     .then(res => {
-    return {
-      props: {...res.data.documents},
-    }})
+      return {
+        props: {...res.data.documents},
+      };
+    })
     .catch(err => console.log(err))
   return {
     props: data
