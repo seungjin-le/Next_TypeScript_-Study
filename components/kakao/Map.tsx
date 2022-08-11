fff47f2baa61cdf5263ecc3fee4b6747c8f15e78
Seungjin Lee <dltmdwls154@gmail.com>
2022-08-11 19:59:46 +0900

    KakaoMaps 확대 축소 버튼 추가

--- components/kakao/Map.tsx
+++ components/kakao/Map.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Map, MapMarker} from "react-kakao-maps-sdk"
-/*global kakao*/
+import {CButton, CContainer} from '@coreui/react'
 
 
 const Maps = () => {
+  const [mapsLevel, setMapsLevel] = useState(3);
   return (
-        <Map
-          center={{ lat: 33.5563, lng: 126.79581 }}
-          style={{ width: "100%", height: "360px" }}
-        >
-          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-            <div style={{ color: "#000" }}>Hello World!</div>
-          </MapMarker>
-        </Map>
+        <>
+          <Map
+            center={{ lat: 33.5563, lng: 126.79581 }}
+            style={{ width: "100%", height: "360px" }}
+            level={mapsLevel}
+          >
+            <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+              <div style={{ color: "#000" }}>Hello World!</div>
+            </MapMarker>
+          </Map>
+          <CContainer>
+            <CButton onClick={() => setMapsLevel(mapsLevel -1)}> 확대 </CButton>
+            <CButton onClick={() => setMapsLevel(mapsLevel +1)}> 축소 </CButton>
+          </CContainer>
+        </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import {CContainer} from '@coreui/react'
+import Kakao from './kakao'
 
 const Home: NextPage = () => {
   return (
    <CContainer>
      <h1>Home</h1>
+     <Kakao />
    </CContainer>
   )
 }
